3b0a47ccbbdd980bdfc7b0b05f2a9c122472abce
Zander Moss <zander@mit.edu>
2016-02-02 20:43:55 -0500

    Ready to push

---
+++ pyclient/Reconciliation.py
@@ -0,0 +1,11 @@
+import Verosim
+import numpy as np
+v=Verosim.Verosim(4)
+def owl(arg):
+	sheeparg=np.asarray(arg)
+	print "SHEEP: ",sheeparg[0]
+	print "DOG: ",sheeparg[1]
+	return float(sheeparg[0]*sheeparg[1])
+v.SetDeSolver(owl)
+v.OscillationProbability(3,4)
+
