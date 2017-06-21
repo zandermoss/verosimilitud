b7fa06653c22d0b55f7ba8c168acb0faf9ec2562
arguelles <arguelles@wisc.edu>
2017-06-21 17:13:26 -0500

    added nusquids interface

---
+++ inc/gsl_integrate_wrap.h
@@ -0,0 +1,42 @@
+#ifndef _GSL_INTEGRATE_H
+#define _GSL_INTEGRATE_H
+
+#include <gsl/gsl_integration.h>
+
+class IntegrateWorkspace {
+private:
+
+public:
+    gsl_integration_workspace* ws;
+    IntegrateWorkspace(size_t limit) {
+        ws=gsl_integration_workspace_alloc(limit);
+    }
+    ~IntegrateWorkspace() {
+        gsl_integration_workspace_free(ws);
+    }
+};
+
+template<typename FunctionType>
+double integrate(IntegrateWorkspace& ws, FunctionType f, double a, double b, double acc=1e-7, unsigned int max_iter=5000){
+    double (*wrapper)(double,void*)=[](double x, void* params){
+        FunctionType& f=*static_cast<FunctionType*>(params);
+        return(f(x));
+    };
+
+    double result, error;
+    gsl_function F;
+    F.function = wrapper;
+    F.params = &f;
+
+    gsl_integration_qags(&F, a, b, 0, acc, max_iter, ws.ws, &result, &error);
+
+    return(result);
+}
+
+template<typename FunctionType>
+double integrate(FunctionType f, double a, double b, double acc=1e-7, unsigned int max_iter=5000){
+    IntegrateWorkspace ws(5000);
+    return integrate(ws, a, b, acc, max_iter);
+}
+
+#endif
